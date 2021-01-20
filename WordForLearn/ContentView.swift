651ac8495dc5e787807cecfc7cab281aabc4ca9e
Nazmul Hasan <nazmulcsharp@gmail.com>
2021-01-20 18:36:53 +0600

    card design done

--- WordForLearn/ContentView.swift
+++ WordForLearn/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      
+        VStack(alignment: .leading, spacing: 0.4, content: {
+            Spacer()
+            HStack{
+                Spacer()
+                Image("Illustration1").resizable().aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text("SwiftUI for ios 14").fontWeight(.bold)
+            Text("20 Secton Spacer()   ").font(.footnote)
+        })
+       
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 20)
         
         
           
@@ -20,5 +34,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView().previewLayout(.fixed(width: 200.0, height: 200.0))
+        
     }
 }
