05dcf24b40e66cd3211fd7e233e877c508955c24
Nazmul Hasan <nazmulcsharp@gmail.com>
2021-01-20 15:45:43 +0600

    Initial Commit

---
+++ WordForLearn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordForLearn
+//
+//  Created by Nazmul Hasan on 20/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
